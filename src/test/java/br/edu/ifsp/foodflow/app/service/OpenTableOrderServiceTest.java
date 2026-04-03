9df0008b0c24250434702e0cf077458c392832ab
NicolasFerezini <nikinhaferezini14@gmail.com>
2026-04-03 19:09:09 -0300

    test: lançar IllegalArgumentException quando ID da mesa não existir

--- src/test/java/br/edu/ifsp/foodflow/app/service/OpenTableOrderServiceTest.java
+++ src/test/java/br/edu/ifsp/foodflow/app/service/OpenTableOrderServiceTest.java
@@ -1,15 +1,25 @@
 package br.edu.ifsp.foodflow.app.service;
 
+import br.edu.ifsp.foodflow.app.domain.table.TableRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class OpenTableOrderServiceTest {
+    @InjectMocks
     private OpenTableOrderService service;
+
+    @Mock
+    private TableRepository tableRepository;
 
     @Test
     @DisplayName("Deve lançar IllegalArgumentException quando o ID da mesa for nulo")
@@ -18,6 +28,17 @@ class OpenTableOrderServiceTest {
                 () -> service.openOrder(null));
 
         assertEquals("O ID da mesa é obrigatório.", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Deve lançar IllegalArgumentException quando a mesa não existir")
+    void shouldThrowExceptionWhenTableDoesNotExist() {
+        when(tableRepository.findById(999)).thenReturn(Optional.empty());
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> service.openOrder(999));
+
+        assertEquals("Mesa não encontrada.", exception.getMessage());
     }
 
 }
