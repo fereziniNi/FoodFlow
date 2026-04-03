bb5ce6f99357430cb42e8eacd4cce66c57807096
NicolasFerezini <nikinhaferezini14@gmail.com>
2026-04-03 19:07:48 -0300

    feat: implementa a função de checar se o id da mesa é nula no openOrder

--- src/main/java/br/edu/ifsp/foodflow/app/service/OpenTableOrderService.java
+++ src/main/java/br/edu/ifsp/foodflow/app/service/OpenTableOrderService.java
@@ -1,4 +1,13 @@
 package br.edu.ifsp.foodflow.app.service;
 
+import br.edu.ifsp.foodflow.app.domain.order.OrderEntity;
+
 public class OpenTableOrderService {
+
+    public OrderEntity openOrder(Integer tableId){
+        if (tableId == null) {
+            throw new IllegalArgumentException("O ID da mesa é obrigatório.");
+        }
+        return null;
+    }
 }
