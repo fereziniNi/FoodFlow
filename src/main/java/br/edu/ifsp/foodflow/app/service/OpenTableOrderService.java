9b6862ab884bdb705a34d3aeab3980083ca05489
NicolasFerezini <nikinhaferezini14@gmail.com>
2026-04-03 19:10:24 -0300

    feat: implementa função de checar se a mesa existe

--- src/main/java/br/edu/ifsp/foodflow/app/service/OpenTableOrderService.java
+++ src/main/java/br/edu/ifsp/foodflow/app/service/OpenTableOrderService.java
@@ -1,13 +1,22 @@
 package br.edu.ifsp.foodflow.app.service;
 
 import br.edu.ifsp.foodflow.app.domain.order.OrderEntity;
+import br.edu.ifsp.foodflow.app.domain.table.TableEntity;
+import br.edu.ifsp.foodflow.app.domain.table.TableRepository;
 
 public class OpenTableOrderService {
+    private final TableRepository tableRepository;
+
+    public OpenTableOrderService(TableRepository tableRepository) {
+        this.tableRepository = tableRepository;
+    }
 
     public OrderEntity openOrder(Integer tableId){
         if (tableId == null) {
             throw new IllegalArgumentException("O ID da mesa é obrigatório.");
         }
+        TableEntity table = tableRepository.findById(tableId)
+                .orElseThrow(() -> new IllegalArgumentException("Mesa não encontrada."));
         return null;
     }
 }
