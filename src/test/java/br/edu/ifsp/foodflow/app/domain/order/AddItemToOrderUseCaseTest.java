9fea3ba1825ccde7612f432f7b0f7e3e71514c42
ricci <riccichristian338@gmail.com>
2026-04-05 23:49:48 -0300

    test: adiciona mock no userRepository

--- src/test/java/br/edu/ifsp/foodflow/app/domain/order/AddItemToOrderUseCaseTest.java
+++ src/test/java/br/edu/ifsp/foodflow/app/domain/order/AddItemToOrderUseCaseTest.java
@@ -1,11 +1,14 @@
 package br.edu.ifsp.foodflow.app.domain.order;
 
+import br.edu.ifsp.foodflow.app.domain.addOn.AddOnRepository;
 import br.edu.ifsp.foodflow.app.domain.menuItem.MenuItemEntity;
 import br.edu.ifsp.foodflow.app.domain.menuItem.MenuItemRepository;
 import br.edu.ifsp.foodflow.app.domain.order.dto.AddItemToOrderRequest;
 import br.edu.ifsp.foodflow.app.domain.order.dto.OrderResponse;
 import br.edu.ifsp.foodflow.app.domain.order.useCases.AddItemToOrderUseCase;
 import br.edu.ifsp.foodflow.app.domain.table.TableEntity;
+import br.edu.ifsp.foodflow.app.domain.user.UserEntity;
+import br.edu.ifsp.foodflow.app.domain.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,29 +25,32 @@ import static org.mockito.Mockito.*;
 public class AddItemToOrderUseCaseTest {
     @Mock private OrderRepository orderRepository;
     @Mock private MenuItemRepository menuItemRepository;
+    @Mock private UserRepository userRepository;
+
     @InjectMocks private AddItemToOrderUseCase sut;
 
     @Test
     void shouldAddItemAnExistingOrder(){
         UUID orderId = UUID.randomUUID();
         UUID menuItemId = UUID.randomUUID();
+        UUID waiterId = UUID.randomUUID();
 
         TableEntity table = new TableEntity(10);
         OrderEntity orderEntity = new OrderEntity(table);
+        UserEntity userEntity = new UserEntity("Estrupicio", "Pereira", "estrupicio@gmail.com", "1234");
 
         MenuItemEntity menuItemEntity = new MenuItemEntity(menuItemId, "X-Tudo", "Ingredientes", 40.0);
-        AddItemToOrderRequest request = new AddItemToOrderRequest(menuItemId, "Sem milho", null, UUID.randomUUID());
+        AddItemToOrderRequest request = new AddItemToOrderRequest(menuItemId, "Sem milho", null, waiterId);
 
         when(orderRepository.findById(orderId)).thenReturn(Optional.of(orderEntity));
         when(menuItemRepository.findById(menuItemId)).thenReturn(Optional.of(menuItemEntity));
+        when(userRepository.findById(waiterId)).thenReturn(Optional.of(userEntity));
 
         OrderResponse response = sut.execute(orderId, request);
 
         assertThat(response).isNotNull();
-
         assertThat(orderEntity.getOrderItems().getFirst().getMenuItem().getName()).isEqualTo("X-Tudo");
         assertThat(orderEntity.getOrderItems().getFirst().getObservations()).isEqualTo("Sem milho");
-
         verify(orderRepository, times(1)).save(orderEntity);
     }
 }
