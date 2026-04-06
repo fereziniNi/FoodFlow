956c6c448fc61bbadb2dc61b072ee8b906824437
fereziniNi <nikinhaferezini14@gmail.com>
2026-04-06 12:39:48 -0300

    fix: ajustando arquivos nas pastas

--- src/test/java/br/edu/ifsp/foodflow/app/domain/order/OpenTableOrderServiceTest.java
+++ src/test/java/br/edu/ifsp/foodflow/app/domain/order/OpenTableOrderServiceTest.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;t
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
