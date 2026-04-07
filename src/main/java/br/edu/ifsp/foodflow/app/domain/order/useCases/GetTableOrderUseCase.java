ecf64545812c0db6bcae951fd41701d45a009ef6
fereziniNi <nikinhaferezini14@gmail.com>
2026-04-07 17:44:43 -0300

    feat: implementa exceção se id do pedido for nulo

--- src/main/java/br/edu/ifsp/foodflow/app/domain/order/useCases/GetTableOrderUseCase.java
+++ src/main/java/br/edu/ifsp/foodflow/app/domain/order/useCases/GetTableOrderUseCase.java
@@ -1,4 +1,13 @@
 package br.edu.ifsp.foodflow.app.domain.order.useCases;
 
+import br.edu.ifsp.foodflow.app.domain.order.OrderEntity;
+
+import java.util.UUID;
+
 public class GetTableOrderUseCase {
+
+    public OrderEntity getOrderById(UUID orderId){
+        if(orderId == null) throw new IllegalArgumentException("O ID da mesa é obrigatório.");
+        return null;
+    }
 }
