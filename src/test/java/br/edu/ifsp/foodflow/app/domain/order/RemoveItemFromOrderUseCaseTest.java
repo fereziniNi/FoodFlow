b2d1f608005026a1813659d867a62833e8003604
ricci <riccichristian338@gmail.com>
2026-04-08 11:03:45 -0300

    test: cenário de teste para não remover item já em preparo ou finalizado

--- src/test/java/br/edu/ifsp/foodflow/app/domain/order/RemoveItemFromOrderUseCaseTest.java
+++ src/test/java/br/edu/ifsp/foodflow/app/domain/order/RemoveItemFromOrderUseCaseTest.java
@@ -25,15 +25,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class RemoveItemFromOrderUseCaseTest {
     private UUID orderId;
-    private UUID menuItemId;
-    private UUID otherMenuItemId;
+    private UUID orderItemId;
+    private UUID otherOrderItemId;
 
     @Mock private OrderRepository orderRepository;
     @Mock private OrderItemRepository orderItemRepository;
@@ -42,8 +41,8 @@ public class RemoveItemFromOrderUseCaseTest {
     @BeforeEach
     void setup(){
         orderId = UUID.randomUUID();
-        menuItemId = UUID.randomUUID();
-        otherMenuItemId = UUID.randomUUID();
+        orderItemId = UUID.randomUUID();
+        otherOrderItemId = UUID.randomUUID();
     }
 
     @Test
@@ -53,8 +52,8 @@ public class RemoveItemFromOrderUseCaseTest {
         MenuItemEntity xBurguer = new MenuItemEntity(UUID.randomUUID(), "X-Burguer", "Delicioso", 20.0, 10);
         MenuItemEntity xTudo = new MenuItemEntity(UUID.randomUUID(), "X-Tudo", "Monstro", 35.0, 5);
 
-        OrderItemEntity itemXburguer = new OrderItemEntity(menuItemId, xBurguer, new ArrayList<>(), null, "");
-        OrderItemEntity itemXtudo = new OrderItemEntity(otherMenuItemId, xTudo, new ArrayList<>(), null, "");
+        OrderItemEntity itemXburguer = new OrderItemEntity(orderItemId, xBurguer, new ArrayList<>(), null, "");
+        OrderItemEntity itemXtudo = new OrderItemEntity(otherOrderItemId, xTudo, new ArrayList<>(), null, "");
 
         List<OrderItemEntity> initialItems = new ArrayList<>(List.of(itemXtudo, itemXburguer));
 
@@ -64,19 +63,47 @@ public class RemoveItemFromOrderUseCaseTest {
         OrderEntity order = new OrderEntity(orderId, table, initialItems, LocalDateTime.now(), true, waiter);
 
         when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
-        when(orderItemRepository.findById(menuItemId)).thenReturn(Optional.of(itemXburguer));
+        when(orderItemRepository.findById(orderItemId)).thenReturn(Optional.of(itemXburguer));
 
         assertEquals(2, order.getOrderItems().size());
         assertEquals(55, order.getTotalPriceOfOrder());
 
-        sut.execute(new RemoveItemFromOrderRequest(orderId, menuItemId));
+        sut.execute(new RemoveItemFromOrderRequest(orderId, orderItemId));
 
         assertEquals(1, order.getOrderItems().size());
         assertEquals(35, order.getTotalPriceOfOrder());
 
         assertFalse(order.getOrderItems().stream()
-                .anyMatch(item -> item.getMenuItem().getId().equals(menuItemId)));
+                .anyMatch(item -> item.getMenuItem().getId().equals(orderItemId)));
 
         verify(orderRepository).save(order);
+    }
+
+    @Test
+    @DisplayName("Dado que um item da comanda esteja marcado como 'em preparo', quando o usuário tentar remover, " +
+                 "o sistema deve bloquear a remoção ou solicitar uma permissão extra para remover.")
+    void shouldThrowExceptionWhenRemovingItemInPreparation(){
+        MenuItemEntity xBurguer = new MenuItemEntity(UUID.randomUUID(), "X-Burguer", "Delicioso", 20.0, 10);
+
+        OrderItemEntity itemInPreparation = new OrderItemEntity(orderItemId, xBurguer, new ArrayList<>(), null, "");
+
+        TableEntity table = new TableEntity(10);
+        UserEntity waiter = mock(UserEntity.class);
+
+        List<OrderItemEntity> initialItems = new ArrayList<>(List.of(itemInPreparation));
+        OrderEntity order = new OrderEntity(orderId, table, initialItems, LocalDateTime.now(), true, waiter);
+
+        when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        when(orderItemRepository.findById(orderItemId)).thenReturn(Optional.of(itemInPreparation));
+
+        RemoveItemFromOrderRequest request = new RemoveItemFromOrderRequest(orderId, orderItemId);
+
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> {
+            sut.execute(request);
+        });
+
+        assertEquals("Não é possível remover um item que já está em preparo ou finalizado.", exception.getMessage());
+
+        verify(orderRepository, never()).save(any());
     }
 }
