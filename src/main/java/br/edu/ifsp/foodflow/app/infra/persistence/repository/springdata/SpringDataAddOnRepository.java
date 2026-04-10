56a074ca540a8d7ae5fdd8a502a9164b4a4b46f0
montillha <marianaalmeida235b@gmail.com>
2026-04-10 02:23:16 -0300

    fix: remove declaração desnecessária de findAllById

--- src/main/java/br/edu/ifsp/foodflow/app/infra/persistence/repository/springdata/SpringDataAddOnRepository.java
+++ src/main/java/br/edu/ifsp/foodflow/app/infra/persistence/repository/springdata/SpringDataAddOnRepository.java
@@ -3,9 +3,7 @@ package br.edu.ifsp.foodflow.app.infra.persistence.repository.springdata;
 import br.edu.ifsp.foodflow.app.infra.persistence.entity.AddOnJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface SpringDataAddOnRepository extends JpaRepository<AddOnJpaEntity, UUID> {
-    List<AddOnJpaEntity> findAllById(List<UUID> ids);
 }
