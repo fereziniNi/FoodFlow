5883e75531f9bfdef20eeb5767242bae83652241
montillha <marianaalmeida235b@gmail.com>
2026-04-11 01:57:03 -0300

    feat: verifica se comanda existe

--- src/main/java/br/edu/ifsp/foodflow/app/application/useCases/order/AdvanceOrderItemStatusUseCase.java
+++ src/main/java/br/edu/ifsp/foodflow/app/application/useCases/order/AdvanceOrderItemStatusUseCase.java
@@ -1,11 +1,24 @@
 package br.edu.ifsp.foodflow.app.application.useCases.order;
 
+import br.edu.ifsp.foodflow.app.domain.order.Order;
+import br.edu.ifsp.foodflow.app.domain.order.OrderRepository;
+import br.edu.ifsp.foodflow.app.infra.exceptions.OrderNotFoundException;
+
 import java.util.Objects;
 import java.util.UUID;
 
 public class AdvanceOrderItemStatusUseCase {
+    private  final OrderRepository orderRepository;
+
+    public AdvanceOrderItemStatusUseCase(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
     public void advanceStatus(UUID orderId, UUID itemId){
         Objects.requireNonNull(orderId,"O ID do pedido não pode ser nulo");
         Objects.requireNonNull(itemId,"O ID do item não pode ser nulo");
+
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderNotFoundException("Pedido não encontrada para o ID: " + orderId));
     }
 }
