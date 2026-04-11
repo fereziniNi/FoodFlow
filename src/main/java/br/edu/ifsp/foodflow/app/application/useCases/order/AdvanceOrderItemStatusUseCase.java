c067f531e991a38ab8eee801ddbb49a976f8b552
montillha <marianaalmeida235b@gmail.com>
2026-04-11 01:38:33 -0300

    feat: adiciona metodo advanceStatus com verificação de id de order não nulo

---
+++ src/main/java/br/edu/ifsp/foodflow/app/application/useCases/order/AdvanceOrderItemStatusUseCase.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.foodflow.app.application.useCases.order;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class AdvanceOrderItemStatusUseCase {
+    public void advanceStatus(UUID orderId, UUID itemId){
+        Objects.requireNonNull(orderId,"O ID do pedido não pode ser nulo");
+    }
+}
