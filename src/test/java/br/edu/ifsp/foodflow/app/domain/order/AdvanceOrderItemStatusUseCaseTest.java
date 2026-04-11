b2656a1e7ece74d9a818d19220da6d8ccb149c35
montillha <marianaalmeida235b@gmail.com>
2026-04-11 01:44:58 -0300

    test: adiciona teste para verificar se id do item não é nulo no avanço de status do pedido

--- src/test/java/br/edu/ifsp/foodflow/app/domain/order/AdvanceOrderItemStatusUseCaseTest.java
+++ src/test/java/br/edu/ifsp/foodflow/app/domain/order/AdvanceOrderItemStatusUseCaseTest.java
@@ -20,8 +20,15 @@ class AdvanceOrderItemStatusUseCaseTest {
     @DisplayName("Dado que a comanda informada é nula, quando o garçom tentar avançar o status do item, " +
             "então o sistema deve lançar um erro informando que a comanda não pode ser nula")
     void shouldThrowNullPointerExceptionWhenOrderIdIsNull(){
-        assertThatNullPointerException()
-                .isThrownBy(()->statusUseCaseTest.advanceStatus(null, UUID.randomUUID()));
+        assertThatNullPointerException().isThrownBy(()->statusUseCaseTest.advanceStatus(null, UUID.randomUUID()));
+
+    }
+
+    @Test
+    @DisplayName("Dado que o id do item informado é nulo, quando o garçom tentar avançar o status, então o sistema deve" +
+            " lançar um erro informando que o id do item não pode ser nulo.")
+    void shouldThrowNullPointerExceptionWhenOrItemIdIsNull(){
+        assertThatNullPointerException().isThrownBy(()->statusUseCaseTest.advanceStatus(UUID.randomUUID(),null));
 
     }
 
