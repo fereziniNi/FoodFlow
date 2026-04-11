9a58c5e730e8cfe8dff0d1c31d739437eded998c
montillha <marianaalmeida235b@gmail.com>
2026-04-11 01:37:26 -0300

    test: adiciona teste para verificar se id da order não é nula no avanço de status do pedido

---
+++ src/test/java/br/edu/ifsp/foodflow/app/domain/order/AdvanceOrderItemStatusUseCaseTest.java
@@ -0,0 +1,31 @@
+package br.edu.ifsp.foodflow.app.domain.order;
+
+
+import br.edu.ifsp.foodflow.app.application.useCases.order.AdvanceOrderItemStatusUseCase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Tag("UnitTest")
+class AdvanceOrderItemStatusUseCaseTest {
+
+    @InjectMocks
+    private AdvanceOrderItemStatusUseCase statusUseCaseTest;
+    @Test
+    @DisplayName("Dado que a comanda informada é nula, quando o garçom tentar avançar o status do item, " +
+            "então o sistema deve lançar um erro informando que a comanda não pode ser nula")
+    void shouldThrowNullPointerExceptionWhenOrderIdIsNull(){
+        assertThatNullPointerException()
+                .isThrownBy(()->statusUseCaseTest.advanceStatus(null, UUID.randomUUID()));
+
+    }
+
+
+
+
+}
