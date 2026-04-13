0d77ae3dc2fa8f14cd3e504a3ae68938e968fa66
fereziniNi <nikinhaferezini14@gmail.com>
2026-04-13 10:30:04 -0300

    refactor: refatorando response para retornar status do item

--- src/main/java/br/edu/ifsp/foodflow/app/web/dtos/response/OrderItemDetailsResponse.java
+++ src/main/java/br/edu/ifsp/foodflow/app/web/dtos/response/OrderItemDetailsResponse.java
@@ -1,9 +1,12 @@
 package br.edu.ifsp.foodflow.app.web.dtos.response;
 
+import br.edu.ifsp.foodflow.app.domain.orderItem.OrderItemStatus;
+
 import java.util.UUID;
 
 public record OrderItemDetailsResponse(
         UUID id,
         String description,
-        double price
+        double price,
+        OrderItemStatus status
 ) {}
