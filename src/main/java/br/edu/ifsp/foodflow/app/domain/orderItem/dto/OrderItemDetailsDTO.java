8b26c5e607e7730f18fa1af67970c76962c07074
fereziniNi <nikinhaferezini14@gmail.com>
2026-04-13 10:29:41 -0300

    refactor: refatorando DTO para retornar status do item

--- src/main/java/br/edu/ifsp/foodflow/app/domain/orderItem/dto/OrderItemDetailsDTO.java
+++ src/main/java/br/edu/ifsp/foodflow/app/domain/orderItem/dto/OrderItemDetailsDTO.java
@@ -1,9 +1,12 @@
 package br.edu.ifsp.foodflow.app.domain.orderItem.dto;
 
+import br.edu.ifsp.foodflow.app.domain.orderItem.OrderItemStatus;
+
 import java.util.UUID;
 
 public record OrderItemDetailsDTO(
         UUID id,
         String description,
-        double price
+        double price,
+        OrderItemStatus status
 ) {}
