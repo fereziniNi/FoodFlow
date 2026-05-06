9bc9bc514ac736071fae46368546c19540e99172
montillha <marianaalmeida235b@gmail.com>
2026-05-06 19:31:52 -0300

    feat: add swagger annotations to MenuController

--- foodflow/src/main/java/br/edu/ifsp/foodflow/app/web/controllers/MenuController.java
+++ foodflow/src/main/java/br/edu/ifsp/foodflow/app/web/controllers/MenuController.java
@@ -4,6 +4,11 @@ import br.edu.ifsp.foodflow.app.application.useCases.addOn.ListAddOnsUseCase;
 import br.edu.ifsp.foodflow.app.application.useCases.menuItem.ListMenuItemsUseCase;
 import br.edu.ifsp.foodflow.app.domain.addOn.AddOn;
 import br.edu.ifsp.foodflow.app.domain.menuItem.MenuItem;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +18,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/menu")
+@Tag(name = "Cardápio", description = "Consulta de itens do cardápio e adicionais")
+@SecurityRequirement(name = "bearer-jwt")
 public class MenuController {
     private final ListMenuItemsUseCase listMenuItemsUseCase;
     private final ListAddOnsUseCase listAddOnsUseCase;
@@ -22,11 +29,19 @@ public class MenuController {
         this.listAddOnsUseCase = listAddOnsUseCase;
     }
 
+    @Operation(summary = "Listar todos os itens do cardápio")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Lista de itens retornada com sucesso")
+    })
     @GetMapping("/items")
     public ResponseEntity<List<MenuItem>> listItems() {
         return ResponseEntity.ok(listMenuItemsUseCase.execute());
     }
 
+    @Operation(summary = "Listar todos os adicionais disponíveis")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Lista de adicionais retornada com sucesso")
+    })
     @GetMapping("/addons")
     public ResponseEntity<List<AddOn>> listAddOns() {
         return ResponseEntity.ok(listAddOnsUseCase.execute());
