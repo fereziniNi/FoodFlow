b05f04dbf8009ffe3e5d171c530cf30eb4048e9c
montillha <marianaalmeida235b@gmail.com>
2026-05-06 19:34:31 -0300

    feat: add swagger annotations to TableController

--- foodflow/src/main/java/br/edu/ifsp/foodflow/app/web/controllers/TableController.java
+++ foodflow/src/main/java/br/edu/ifsp/foodflow/app/web/controllers/TableController.java
@@ -2,6 +2,11 @@ package br.edu.ifsp.foodflow.app.web.controllers;
 
 import br.edu.ifsp.foodflow.app.application.useCases.table.ListTablesUseCase;
 import br.edu.ifsp.foodflow.app.web.dtos.response.TableResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +16,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/tables")
+@Tag(name = "Mesas", description = "Consulta de mesas do restaurante")
+@SecurityRequirement(name = "bearer-jwt")
 public class TableController {
     private final ListTablesUseCase listTablesUseCase;
 
@@ -18,6 +25,10 @@ public class TableController {
         this.listTablesUseCase = listTablesUseCase;
     }
 
+    @Operation(summary = "Listar todas as mesas")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "Lista de mesas retornada com sucesso")
+    })
     @GetMapping
     public ResponseEntity<List<TableResponse>> listAll() {
         List<TableResponse> response = listTablesUseCase.execute()
