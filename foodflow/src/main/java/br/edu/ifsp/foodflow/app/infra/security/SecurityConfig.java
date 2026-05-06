8861e3f1e207d9cdd41850f7b59b4f06954fbe1b
montillha <marianaalmeida235b@gmail.com>
2026-05-06 19:25:24 -0300

    feat: expose swagger ui routes without authentication

--- foodflow/src/main/java/br/edu/ifsp/foodflow/app/infra/security/SecurityConfig.java
+++ foodflow/src/main/java/br/edu/ifsp/foodflow/app/infra/security/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
