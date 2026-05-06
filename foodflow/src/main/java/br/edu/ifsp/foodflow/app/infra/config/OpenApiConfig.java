530f0f7e49ddb084acb58796e5dcb913c28e543a
montillha <marianaalmeida235b@gmail.com>
2026-05-06 19:23:26 -0300

    feat: add OpenAPI config with JWT security scheme

---
+++ foodflow/src/main/java/br/edu/ifsp/foodflow/app/infra/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package br.edu.ifsp.foodflow.app.infra.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI foodFlowOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("FoodFlow API")
+                        .description("API de gerenciamento de comandas e pedidos de restaurante")
+                        .version("1.0.0"))
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
