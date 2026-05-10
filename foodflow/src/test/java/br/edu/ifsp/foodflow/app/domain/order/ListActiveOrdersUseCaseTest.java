fab5f24a939eb2d882353079b243d9cb2b55e62f
xChristianCRX <riccichristian338@gmail.com>
2026-05-10 19:16:38 -0300

    test: testa retorno dos itens ativos quando não se passa um garçom

---
+++ foodflow/src/test/java/br/edu/ifsp/foodflow/app/domain/order/ListActiveOrdersUseCaseTest.java
@@ -0,0 +1,65 @@
+package br.edu.ifsp.foodflow.app.domain.order;
+
+import br.edu.ifsp.foodflow.app.application.useCases.order.ListActiveOrdersUseCase;
+import br.edu.ifsp.foodflow.app.domain.menuItem.MenuItem;
+import br.edu.ifsp.foodflow.app.domain.order.dto.OrderDetailsDTO;
+import br.edu.ifsp.foodflow.app.domain.orderItem.OrderItem;
+import br.edu.ifsp.foodflow.app.domain.table.Table;
+import br.edu.ifsp.foodflow.app.domain.user.User;
+import br.edu.ifsp.foodflow.app.domain.user.UserRole;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
+
+@Tag("UnitTest")
+@ExtendWith(MockitoExtension.class)
+public class ListActiveOrdersUseCaseTest {
+
+    @Mock
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private ListActiveOrdersUseCase sut;
+
+    @Tag("Structural")
+    @Nested
+    @DisplayName("Testes Estruturais (com instâncias reais)")
+    class StructuralTests {
+        @Test
+        @DisplayName("Deve mapear corretamente um pedido quando o item não tiver um garçom responsável, atribuindo a string 'Sistema'")
+        void shouldMapOrderAndItemsWhenItemHasNoWaiter() {
+            Table table = new Table(2);
+            User waiter = new User("Joao", "Garcom", "joao@email.com", "123", UserRole.WAITER);
+            Order order = new Order(table, waiter);
+
+            MenuItem menuItem = new MenuItem(UUID.randomUUID(), "Refrigerante", "Lata 350ml", 10.0, 0);
+
+            OrderItem item = new OrderItem(UUID.randomUUID(), menuItem, List.of(), null, "Com gelo");
+            order.addOrderItem(item);
+
+            when(orderRepository.findAllActive()).thenReturn(List.of(order));
+
+            List<OrderDetailsDTO> result = sut.execute();
+
+            assertThat(result).isNotNull();
+            assertThat(result.size()).isEqualTo(1);
+
+            OrderDetailsDTO orderDetails = result.getFirst();
+            assertThat(orderDetails.items().size()).isEqualTo(1);
+            assertThat(orderDetails.items().getFirst().waiterName()).isEqualTo("Sistema");
+
+            verify(orderRepository, times(1)).findAllActive();
+        }
+    }
+}
