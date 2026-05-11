74b4bcfc0aaa1a1fc972701770a1cf199937d449
montillha <marianaalmeida235b@gmail.com>
2026-05-11 15:44:18 -0300

    test: refatora setup com @BeforeEach e adiciona teste de mutação para adicionais em ListActiveOrders

--- foodflow/src/test/java/br/edu/ifsp/foodflow/app/application/useCases/order/ListActiveOrdersUseCaseTest.java
+++ foodflow/src/test/java/br/edu/ifsp/foodflow/app/application/useCases/order/ListActiveOrdersUseCaseTest.java
@@ -1,17 +1,16 @@
 package br.edu.ifsp.foodflow.app.application.useCases.order;
 
+import br.edu.ifsp.foodflow.app.domain.addOn.AddOn;
 import br.edu.ifsp.foodflow.app.domain.menuItem.MenuItem;
 import br.edu.ifsp.foodflow.app.domain.order.Order;
 import br.edu.ifsp.foodflow.app.domain.order.OrderRepository;
 import br.edu.ifsp.foodflow.app.domain.order.dto.OrderDetailsDTO;
 import br.edu.ifsp.foodflow.app.domain.orderItem.OrderItem;
+import br.edu.ifsp.foodflow.app.domain.orderItem.dto.AddOnSummaryDTO;
 import br.edu.ifsp.foodflow.app.domain.table.Table;
 import br.edu.ifsp.foodflow.app.domain.user.User;
 import br.edu.ifsp.foodflow.app.domain.user.UserRole;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -26,6 +25,18 @@ import static org.mockito.Mockito.*;
 @Tag("UnitTest")
 @ExtendWith(MockitoExtension.class)
 public class ListActiveOrdersUseCaseTest {
+    private User waiter;
+    private Table table;
+    private Order order;
+    private MenuItem menuItem;
+
+    @BeforeEach
+    void setUp(){
+        waiter = new User("Joao", "joao", "joao@email.com", "123", UserRole.WAITER);
+        table = new Table(2);
+        order = new Order(table,waiter);
+        menuItem = new MenuItem(UUID.randomUUID(), "X-Bacon", "Ingredientes", 35.0, 15);
+    }
 
     @Mock
     private OrderRepository orderRepository;
@@ -40,11 +51,6 @@ public class ListActiveOrdersUseCaseTest {
         @Test
         @DisplayName("Deve mapear corretamente um pedido quando o item não tiver um garçom responsável, atribuindo a string 'Sistema'")
         void shouldMapOrderAndItemsWhenItemHasNoWaiter() {
-            Table table = new Table(2);
-            User waiter = new User("Joao", "Garcom", "joao@email.com", "123", UserRole.WAITER);
-            Order order = new Order(table, waiter);
-
-            MenuItem menuItem = new MenuItem(UUID.randomUUID(), "Refrigerante", "Lata 350ml", 10.0, 0);
 
             OrderItem item = new OrderItem(UUID.randomUUID(), menuItem, List.of(), null, "Com gelo");
             order.addOrderItem(item);
@@ -67,12 +73,6 @@ public class ListActiveOrdersUseCaseTest {
     @Test
     @DisplayName("Deve mapear corretamente um pedido e seus itens quando o item possuir um garçom responsável")
     void shouldMapOrderAndItemsWhenItemHasWaiter() {
-        Table table = new Table(5);
-        User waiter = new User("Joao", "joao", "joao@email.com", "123", UserRole.WAITER);
-        Order order = new Order(table, waiter);
-
-        MenuItem menuItem = new MenuItem(UUID.randomUUID(), "X-Bacon", "Ingredientes", 35.0, 15);
-
         OrderItem item = new OrderItem(UUID.randomUUID(), menuItem, List.of(), waiter, "Sem cebola");
         order.addOrderItem(item);
 
@@ -84,7 +84,7 @@ public class ListActiveOrdersUseCaseTest {
         assertThat(result.size()).isEqualTo(1);
 
         OrderDetailsDTO orderDetails = result.getFirst();
-        assertThat(orderDetails.tableNumber()).isEqualTo(5);
+        assertThat(orderDetails.tableNumber()).isEqualTo(table.getTableNumber());
         assertThat(orderDetails.userName()).isEqualTo("joao");
         assertThat(orderDetails.total()).isEqualTo(35.0);
 
@@ -92,5 +92,33 @@ public class ListActiveOrdersUseCaseTest {
         assertThat(orderDetails.items().getFirst().waiterName()).isEqualTo("joao");
 
         verify(orderRepository, times(1)).findAllActive();
+    }
+
+
+    @Nested
+    @Tag("Mutation")
+    @DisplayName("Testes de Mutação")
+    class MutationTests {
+        @Test
+        @DisplayName("Deve mapear corretamente um pedido que possui itens com adicional")
+        void shouldMapOrderAndItemsWhenItemHasAdditional() {
+
+            List<AddOn> addOns = List.of(
+                    new AddOn("Bacon extra", 5.0),
+                    new AddOn("Queijo Extra", 3.0)
+            );
+
+            OrderItem item = new OrderItem(UUID.randomUUID(), menuItem, addOns, waiter, "Sem cebola");
+            order.addOrderItem(item);
+
+            when(orderRepository.findAllActive()).thenReturn(List.of(order));
+
+            List<OrderDetailsDTO> result = sut.execute();
+            OrderDetailsDTO orderDetails = result.getFirst();
+
+            assertThat(orderDetails.items().getFirst().additions()).isEqualTo(
+                    addOns.stream().map(addOn -> new AddOnSummaryDTO(addOn.getName(), addOn.getPrice())).toList());
+
+        }
     }
 }
